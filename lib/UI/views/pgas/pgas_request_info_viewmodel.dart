b6a70dd2bcdc994077fbae60dc906dadc1ef967d
ender <enderclub12@gmail.com>
2022-04-20 21:10:43 +0700

    Экран с информацией о заявке на ПГАС

---
+++ lib/UI/views/pgas/pgas_request_info_viewmodel.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:stacked/stacked.dart';
+import 'package:http/http.dart' as http;
+
+import 'edit_pgas_request_screen.dart';
+import 'model/pgas_detail.dart';
+
+class PgasRequestInfoViewModel extends BaseViewModel {
+  PgasRequestInfoViewModel(BuildContext context);
+
+  FlutterSecureStorage storage = const FlutterSecureStorage();
+  bool circle = true;
+  PgasDetailModel? pgasRequest;
+
+  Future onReady(context) async {
+    await fetchDetailPgasRequest(context);
+    circle = false;
+  }
+
+  goToEditPgasRequestScreen(context) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => const EditPgasRequestScreen())).then((value) => onGoBack(context));
+  }
+
+  void refreshData(context) async {
+    await fetchDetailPgasRequest(context);
+  }
+
+  FutureOr onGoBack(context) {
+    refreshData(context);
+  }
+
+  fetchDetailPgasRequest(context) async {
+    String? eiosAccessToken = await storage.read(key: "tokenKey");
+    String? pgasRequestId = await storage.read(key: "pgas_id");
+    Map<String, String> header = {
+      "X-Access-Token": "$eiosAccessToken"
+    };
+    Map<String, dynamic> body = {
+      "requestId": pgasRequestId
+    };
+    var response = await http.post(Uri.parse("https://api-next.kemsu.ru/api/student-depatment/pgas-mobile/getRequestInfo"), headers: header, body: body);
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      pgasRequest = PgasDetailModel.fromJson(json.decode(response.body)["result"]);
+      notifyListeners();
+    }
+  }
+
+  deletePgasAction(context) async {
+    String? eiosAccessToken = await storage.read(key: "tokenKey");
+    String? pgasRequestId = await storage.read(key: "pgas_id");
+    Map<String, String> header = {
+      "X-Access-Token": "$eiosAccessToken"
+    };
+    Map<String, dynamic> body = {
+      "requestId": pgasRequestId
+    };
+    var response = await http.post(Uri.parse("https://api-next.kemsu.ru/api/student-depatment/pgas-mobile/deleteRequest"), headers: header, body: body);
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      Navigator.popUntil(context, ModalRoute.withName("PgasList"));
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Заявка успешно удалена.")));
+      notifyListeners();
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Ошибка при удалении заявки.")));
+      print(response.statusCode);
+      print(response.body);
+    }
+  }
+}
