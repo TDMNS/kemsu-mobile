11e3cb1e7f7882231ba53bbceb75396eab8aa2ac
ender <enderclub12@gmail.com>
2022-06-03 00:45:59 +0700

    added file picker package

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -4,7 +4,9 @@
 
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
 
+import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
@@ -12,6 +14,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
+  FilePickerWeb.registerWith(registrar);
   FlutterSecureStorageWeb.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
