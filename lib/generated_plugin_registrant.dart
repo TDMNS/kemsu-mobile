687a5c5d83b31b2b148933d17be209dc2350b877
TDMNS <773o@mail.ru>
2022-06-03 09:26:04 +0700

    [PGAS] Fix paths for merge request

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -4,7 +4,6 @@
 
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
-// ignore_for_file: depend_on_referenced_packages
 
 import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
