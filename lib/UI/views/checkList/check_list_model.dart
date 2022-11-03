ece04101dc49f125cd6d3b80109edd5e065b212f
Oleg Kolbasa <773o@mail.ru>
2022-11-04 04:22:13 +0700

    Add spaces into check module

--- lib/UI/views/checkList/check_list_model.dart
+++ lib/UI/views/checkList/check_list_model.dart
@@ -4,19 +4,23 @@ class CheckList {
   String? debt;
   String? commentary;
 
-  CheckList(
-      {this.groupName, this.departmentTitle, this.debt, this.commentary});
+  CheckList({
+    this.groupName,
+    this.departmentTitle,
+    this.debt,
+    this.commentary
+  });
 
   CheckList.fromJson(Map<String, dynamic> json) {
     groupName = json["groupName"];
     departmentTitle = json["departmentTitle"];
-    if(json["commentary"]!=null) {
+    if (json["commentary"] != null) {
       commentary = json["commentary"];
     } else {
       commentary = "";
     }
     debt="";
-    if(json["noDebtFlag"]==1) debt="Нет";
-    if(json["debtFlag"]==1) debt="Есть";
+    if (json["noDebtFlag"] == 1) debt = "Нет";
+    if (json["debtFlag"] == 1) debt = "Есть";
   }
 }
