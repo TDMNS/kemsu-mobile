747bc81e07b02e8fc6746f908089275dca7ec326
TDMNS <773o@mail.ru>
2023-03-20 09:18:17 +0700

    Change purple colors to blue in theme

--- lib/UI/DarkTheme.dart
+++ lib/UI/DarkTheme.dart
@@ -3,28 +3,22 @@ import 'package:flutter/material.dart';
 class MyThemes {
   static final darkTheme = ThemeData(
       scaffoldBackgroundColor: Colors.black54,
-      accentColor: Colors.black,
       dividerColor: Colors.transparent,
       primaryColor: Colors.grey.shade900,
       focusColor: Colors.white,
-      textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.white),
       inputDecorationTheme: const InputDecorationTheme(
           iconColor: Colors.white,
-          focusedBorder:
-              UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
           fillColor: Colors.white,
           focusColor: Colors.white,
-          enabledBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.white))),
+          enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white))),
       primaryColorDark: Colors.white,
       primaryColorLight: Colors.blueGrey.shade900.withOpacity(0.3),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.blue))),
+      elevatedButtonTheme:
+          ElevatedButtonThemeData(style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue))),
       canvasColor: Colors.white,
-      appBarTheme: AppBarTheme(
-          backgroundColor: Colors.grey.shade900,
-          titleTextStyle: const TextStyle(color: Colors.white)),
+      appBarTheme:
+          AppBarTheme(backgroundColor: Colors.grey.shade900, titleTextStyle: const TextStyle(color: Colors.white)),
       cardColor: Colors.grey.shade900,
       expansionTileTheme: const ExpansionTileThemeData(
         //backgroundColor: Colors.grey.shade900,
@@ -33,33 +27,27 @@ class MyThemes {
         iconColor: Colors.blue,
         //collapsedBackgroundColor: Colors.grey.shade900,
       ),
-      colorScheme: const ColorScheme.dark());
+      colorScheme: const ColorScheme.dark().copyWith(secondary: Colors.black),
+      textSelectionTheme: const TextSelectionThemeData(selectionHandleColor: Colors.blue, selectionColor: Colors.blue));
 
   static final lightTheme = ThemeData(
       scaffoldBackgroundColor: Colors.white,
-      radioTheme:
-          RadioThemeData(fillColor: MaterialStateProperty.all(Colors.blue)),
+      radioTheme: RadioThemeData(fillColor: MaterialStateProperty.all(Colors.blue)),
       dividerColor: Colors.transparent,
       primaryColor: Colors.white,
-      accentColor: Colors.white,
       primaryColorDark: Colors.black,
       primaryColorLight: Colors.grey.withOpacity(0.4),
       focusColor: Colors.black,
       inputDecorationTheme: const InputDecorationTheme(
           iconColor: Colors.black,
-          focusedBorder:
-              UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+          focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
           fillColor: Colors.black,
           focusColor: Colors.black,
-          enabledBorder: UnderlineInputBorder(
-              borderSide: BorderSide(color: Colors.black))),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.blue))),
+          enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black))),
+      elevatedButtonTheme:
+          ElevatedButtonThemeData(style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue))),
       canvasColor: Colors.grey.shade600,
-      appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          titleTextStyle: TextStyle(color: Colors.black)),
+      appBarTheme: const AppBarTheme(backgroundColor: Colors.white, titleTextStyle: TextStyle(color: Colors.black)),
       cardColor: Colors.white,
       expansionTileTheme: const ExpansionTileThemeData(
         //backgroundColor: Colors.white,
@@ -67,5 +55,6 @@ class MyThemes {
         collapsedIconColor: Colors.black,
         iconColor: Colors.blue,
       ),
-      colorScheme: const ColorScheme.light());
+      colorScheme: const ColorScheme.light().copyWith(secondary: Colors.white),
+      textSelectionTheme: const TextSelectionThemeData(selectionHandleColor: Colors.blue, selectionColor: Colors.blue));
 }
