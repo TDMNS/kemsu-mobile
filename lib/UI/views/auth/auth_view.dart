65c36323d1c6cedbe7a75ba7a77d211229d5dfc3
Oleg Kolbasa <773o@mail.ru>
2023-07-25 23:25:57 +0700

    Small fixes with text fields

--- lib/UI/views/auth/auth_view.dart
+++ lib/UI/views/auth/auth_view.dart
@@ -39,120 +39,125 @@ class AuthView extends StatelessWidget {
   }
 
   Widget _buildAuthView(BuildContext context, AuthViewModel model) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Theme.of(context).primaryColor == Colors.grey.shade900
-            ? Image.asset(
-                'images/logo_dark.png',
-                height: 120,
-              )
-            : Image.asset(
-                'images/logo_light.png',
-                height: 120,
-              ),
-        const SizedBox(height: 50),
-        Container(
-          margin: const EdgeInsets.only(right: 15, left: 15, bottom: 8, top: 8),
-          child: TextFormField(
-            onChanged: (letters) {
-              model.notifyListeners();
-            },
-            decoration: InputDecoration(
-              contentPadding: const EdgeInsets.only(left: 15, top: 15),
-              suffixIcon: model.loginController.text.isNotEmpty ? IconButton(icon: const Icon(Icons.cancel), onPressed: () {
-                model.loginController.clear();
+    return ListView(
+      children: [
+        Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(height: 30),
+          Theme.of(context).primaryColor == Colors.grey.shade900
+              ? Image.asset(
+                  'images/logo_dark.png',
+                  height: 120,
+                )
+              : Image.asset(
+                  'images/logo_light.png',
+                  height: 120,
+                ),
+          const SizedBox(height: 30),
+          Text('Войти в КемГУ', style: TextStyle(fontFamily: "Ubuntu", color: Theme.of(context).primaryColorDark, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 8),
+          Container(
+            margin: const EdgeInsets.only(right: 15, left: 15, bottom: 8, top: 8),
+            child: TextFormField(
+              onChanged: (letters) {
                 model.notifyListeners();
-              }) : null,
-              hintText: 'Логин',
-              hintStyle: const TextStyle(fontFamily: "Ubuntu", color: Colors.grey, fontWeight: FontWeight.bold),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12.0),
-                borderSide: const BorderSide(color: Colors.grey),
+              },
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.only(left: 15, top: 15),
+                suffixIcon: model.loginController.text.isNotEmpty ? IconButton(icon: const Icon(Icons.cancel), onPressed: () {
+                  model.loginController.clear();
+                  model.notifyListeners();
+                }) : null,
+                hintText: 'Логин',
+                hintStyle: const TextStyle(fontFamily: "Ubuntu", color: Colors.grey, fontWeight: FontWeight.bold),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                  borderSide: const BorderSide(color: Colors.grey),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                  borderSide: const BorderSide(color: Colors.blue),
+                )
               ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12.0),
-                borderSide: const BorderSide(color: Colors.blue),
-              )
-            ),
-            style: const TextStyle(fontFamily: "Ubuntu", fontWeight: FontWeight.bold),
-            controller: model.loginController,
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.only(right: 15, left: 15, bottom: 8, top: 8),
-          color: Colors.grey.withOpacity(0.05),
-          child: TextFormField(
-            onChanged: (letters) {
-              model.notifyListeners();
-            },
-            decoration: InputDecoration(
-              suffixIcon: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  model.passwordController.text.isNotEmpty
-                      ? IconButton(
-                          onPressed: () {
-                            model.isVisiblePassword();
-                          },
-                          icon: Icon(model.isObscure ? Icons.remove_red_eye : Icons.remove_red_eye_outlined),
-                        )
-                      : const SizedBox()
-                ],
-              ),
-              contentPadding: const EdgeInsets.only(left: 15, top: 15),
-              hintText: 'Введите пароль',
-              hintStyle: const TextStyle(fontFamily: "Ubuntu", color: Colors.grey, fontWeight: FontWeight.bold),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12.0),
-                borderSide: const BorderSide(color: Colors.grey),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12.0),
-                borderSide: const BorderSide(color: Colors.blue),
-              )
-            ),
-            style: const TextStyle(fontFamily: "Ubuntu", fontWeight: FontWeight.bold),
-            controller: model.passwordController,
-            obscureText: model.isObscure,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 3, bottom: 8),
-          child: Row(
-            children: <Widget>[
-              Checkbox(
-                value: model.rememberMe,
-                activeColor: Colors.blue,
-                onChanged: (bool? value) {
-                  model.rememberFunc(value);
-                },
-              ),
-              const Text(
-                'Запомнить меня',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-              ),
-            ],
-          ),
-        ),
-        mainButton(context, onPressed: () {
-          model.authButton(context);
-        }, title: 'Войти', isPrimary: true),
-        Padding(
-          padding: const EdgeInsets.only(top: 35),
-          child: GestureDetector(
-            onTap: () {
-              _authAlert(context);
-            },
-            child: const Text(
-              'Проблемы с входом?',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),
+              style: const TextStyle(fontFamily: "Ubuntu", fontWeight: FontWeight.bold),
+              controller: model.loginController,
             ),
           ),
-        ),
-      ],
-    );
+          Container(
+            margin: const EdgeInsets.only(right: 15, left: 15, bottom: 8, top: 8),
+            child: TextFormField(
+              onChanged: (letters) {
+                model.notifyListeners();
+              },
+              decoration: InputDecoration(
+                suffixIcon: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    model.passwordController.text.isNotEmpty
+                        ? IconButton(
+                            onPressed: () {
+                              model.isVisiblePassword();
+                            },
+                            icon: Icon(model.isObscure ? Icons.remove_red_eye : Icons.remove_red_eye_outlined),
+                          )
+                        : const SizedBox()
+                  ],
+                ),
+                contentPadding: const EdgeInsets.only(left: 15, top: 15),
+                hintText: 'Введите пароль',
+                hintStyle: const TextStyle(fontFamily: "Ubuntu", color: Colors.grey, fontWeight: FontWeight.bold),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                  borderSide: const BorderSide(color: Colors.grey),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                  borderSide: const BorderSide(color: Colors.blue),
+                )
+              ),
+              style: const TextStyle(fontFamily: "Ubuntu", fontWeight: FontWeight.bold),
+              controller: model.passwordController,
+              obscureText: model.isObscure,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 3, bottom: 8),
+            child: Row(
+              children: <Widget>[
+                Checkbox(
+                  value: model.rememberMe,
+                  activeColor: Colors.blue,
+                  onChanged: (bool? value) {
+                    model.rememberFunc(value);
+                  },
+                ),
+                const Text(
+                  'Запомнить меня',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+          mainButton(context, onPressed: () {
+            model.authButton(context);
+          }, title: 'Войти', isPrimary: true),
+          Padding(
+            padding: const EdgeInsets.only(top: 35),
+            child: GestureDetector(
+              onTap: () {
+                _authAlert(context);
+              },
+              child: const Text(
+                'Проблемы с входом?',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.blue),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ]);
   }
 
   void _authAlert(BuildContext context) {
