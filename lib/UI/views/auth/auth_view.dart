d06385faac471609aa891287e6ffaa16359a8486
Oleg Kolbasa <773o@mail.ru>
2023-07-28 10:25:26 +0700

    Delete auto capitalization from text fields

--- lib/UI/views/auth/auth_view.dart
+++ lib/UI/views/auth/auth_view.dart
@@ -91,7 +91,8 @@ class AuthView extends StatelessWidget {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (v) {
                   FocusScope.of(context).requestFocus(model.passwordFocus);
-                }),
+                },
+                textCapitalization: TextCapitalization.none),
           ),
           Container(
             margin: const EdgeInsets.only(right: 15, left: 15, bottom: 8, top: 8),
@@ -132,7 +133,8 @@ class AuthView extends StatelessWidget {
                 autocorrect: false,
                 enableSuggestions: false,
                 keyboardType: TextInputType.visiblePassword,
-                textInputAction: TextInputAction.done),
+                textInputAction: TextInputAction.done,
+                textCapitalization: TextCapitalization.none),
           ),
           Padding(
             padding: const EdgeInsets.only(left: 3, bottom: 8),
