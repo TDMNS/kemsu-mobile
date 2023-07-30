486ef289962f04865ca204c030fb56f877e685c3
Oleg Kolbasa <773o@mail.ru>
2023-07-31 00:02:46 +0700

    Make refactoring in auth screen

--- lib/UI/views/auth/auth_view.dart
+++ lib/UI/views/auth/auth_view.dart
@@ -58,96 +58,53 @@ class AuthView extends StatelessWidget {
           Text(Localizable.applicationLogin,
               style: TextStyle(fontFamily: "Ubuntu", color: Theme.of(context).primaryColorDark, fontWeight: FontWeight.bold)),
           const SizedBox(height: 8),
-          Container(
-            margin: const EdgeInsets.only(right: 15, left: 15, bottom: 8, top: 8),
-            child: TextFormField(
-                focusNode: model.loginFocus,
-                onChanged: (letters) {
-                  model.notifyListeners();
-                },
-                onTap: () {
-                  model.notifyListeners();
-                },
-                decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.only(left: 15, top: 15),
-                    suffixIcon: model.loginController.text.isNotEmpty && model.loginFocus.hasFocus
-                        ? IconButton(
-                            icon: const Icon(Icons.cancel),
-                            onPressed: () {
-                              model.loginController.clear();
-                              model.notifyListeners();
-                            })
-                        : null,
-                    hintText: Localizable.login,
-                    hintStyle: const TextStyle(fontFamily: "Ubuntu", color: Colors.grey, fontWeight: FontWeight.bold),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      borderSide: const BorderSide(color: Colors.grey),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      borderSide: const BorderSide(color: Colors.blue),
-                    )),
-                style: const TextStyle(fontFamily: "Ubuntu", fontWeight: FontWeight.bold),
-                controller: model.loginController,
-                autocorrect: false,
-                enableSuggestions: false,
-                keyboardType: TextInputType.visiblePassword,
-                textInputAction: TextInputAction.next,
-                onFieldSubmitted: (v) {
-                  FocusScope.of(context).requestFocus(model.passwordFocus);
-                  model.notifyListeners();
-                },
-                textCapitalization: TextCapitalization.none),
-          ),
-          Container(
-            margin: const EdgeInsets.only(right: 15, left: 15, bottom: 8, top: 8),
-            child: TextFormField(
-                focusNode: model.passwordFocus,
-                onChanged: (letters) {
-                  model.notifyListeners();
-                },
-                onTap: () {
-                  model.notifyListeners();
-                },
-                decoration: InputDecoration(
-                    suffixIcon: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        model.passwordController.text.isNotEmpty
-                            ? IconButton(
-                                onPressed: () {
-                                  model.isVisiblePassword();
-                                },
-                                icon: Icon(model.isObscure ? Icons.remove_red_eye : Icons.remove_red_eye_outlined),
-                              )
-                            : const SizedBox()
-                      ],
-                    ),
-                    contentPadding: const EdgeInsets.only(left: 15, top: 15),
-                    hintText: Localizable.enterPassword,
-                    hintStyle: const TextStyle(fontFamily: "Ubuntu", color: Colors.grey, fontWeight: FontWeight.bold),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      borderSide: const BorderSide(color: Colors.grey),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                      borderSide: const BorderSide(color: Colors.blue),
-                    )),
-                style: const TextStyle(fontFamily: "Ubuntu", fontWeight: FontWeight.bold),
-                controller: model.passwordController,
-                obscureText: model.isObscure,
-                autocorrect: false,
-                enableSuggestions: false,
-                keyboardType: TextInputType.visiblePassword,
-                textInputAction: TextInputAction.done,
-                onFieldSubmitted: (v) {
-                  model.notifyListeners();
-                },
-                textCapitalization: TextCapitalization.none),
-          ),
+          _customTextField(
+              context,
+              model.loginFocus,
+              (model.loginController.text.isNotEmpty && model.loginFocus.hasFocus
+                  ? IconButton(
+                      icon: const Icon(Icons.cancel),
+                      onPressed: () {
+                        model.loginController.clear();
+                        model.notifyListeners();
+                      })
+                  : const SizedBox()),
+              model.loginController,
+              false,
+              TextInputAction.next, () {
+            model.notifyListeners();
+          }, () {
+            model.notifyListeners();
+          }, () {
+            FocusScope.of(context).requestFocus(model.passwordFocus);
+            model.notifyListeners();
+          }),
+          _customTextField(
+              context,
+              model.passwordFocus,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  model.passwordController.text.isNotEmpty
+                      ? IconButton(
+                          onPressed: () {
+                            model.isVisiblePassword();
+                          },
+                          icon: Icon(model.isObscure ? Icons.remove_red_eye : Icons.remove_red_eye_outlined),
+                        )
+                      : const SizedBox()
+                ],
+              ),
+              model.passwordController,
+              model.isObscure,
+              TextInputAction.done, () {
+            model.notifyListeners();
+          }, () {
+            model.notifyListeners();
+          }, () {
+            model.notifyListeners();
+          }),
           Padding(
             padding: const EdgeInsets.only(left: 3, bottom: 8),
             child: Row(
@@ -171,14 +128,53 @@ class AuthView extends StatelessWidget {
           }, title: 'Войти', isPrimary: true),
           const SizedBox(height: 20),
           mainButton(context, onPressed: () {
-            _authAlert(context);
+            _customAlert(context);
           }, title: Localizable.troubleLoggingInHeader, isPrimary: false)
         ],
       ),
     ]);
   }
 
-  void _authAlert(BuildContext context) {
+  _customTextField(BuildContext context, FocusNode focusNode, Widget suffixIcon, TextEditingController textEditingController, bool isObscure,
+      TextInputAction textInputAction, onChanged, onTap, onFieldSubmitted) {
+    return Container(
+      margin: const EdgeInsets.only(right: 15, left: 15, bottom: 8, top: 8),
+      child: TextFormField(
+          focusNode: focusNode,
+          onChanged: (letters) {
+            onChanged();
+          },
+          onTap: () {
+            onTap();
+          },
+          decoration: InputDecoration(
+              suffixIcon: suffixIcon,
+              contentPadding: const EdgeInsets.only(left: 15, top: 15),
+              hintText: Localizable.enterPassword,
+              hintStyle: const TextStyle(fontFamily: "Ubuntu", color: Colors.grey, fontWeight: FontWeight.bold),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12.0),
+                borderSide: const BorderSide(color: Colors.grey),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12.0),
+                borderSide: const BorderSide(color: Colors.blue),
+              )),
+          style: const TextStyle(fontFamily: "Ubuntu", fontWeight: FontWeight.bold),
+          controller: textEditingController,
+          obscureText: isObscure,
+          autocorrect: false,
+          enableSuggestions: false,
+          keyboardType: TextInputType.visiblePassword,
+          textInputAction: textInputAction,
+          onFieldSubmitted: (v) {
+            onFieldSubmitted();
+          },
+          textCapitalization: TextCapitalization.none),
+    );
+  }
+
+  void _customAlert(BuildContext context) {
     showDialog<String>(
       context: context,
       builder: (BuildContext context) => AlertDialog(
