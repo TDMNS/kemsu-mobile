e194be30be27f3cfddca167526812e74895364c9
Oleg <773o@mail.ru>
2023-11-27 11:32:55 +0700

    Add auth data writing into storage

--- lib/UI/views/auth/new_auth/auth_bloc.dart
+++ lib/UI/views/auth/new_auth/auth_bloc.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kemsu_app/domain/repositories/authorization/abstract_auth_repository.dart';
 
 import '../../../../domain/models/authorization/auth_model.dart';
+import '../../../splash_screen.dart';
 
 part 'auth_events.dart';
 part 'auth_state.dart';
@@ -18,6 +19,13 @@ class AuthBloc extends Bloc<AuthEvents, AuthState> {
   Future<void> _postAuth(PostAuthEvents event, Emitter<AuthState> emit) async {
     try {
       final authData = await authRepository.postAuth(event.login, event.password);
+
+      await storage.write(key: "tokenKey", value: authData.accessToken);
+      await storage.write(key: "login", value: event.login);
+      await storage.write(key: "password", value: event.password);
+      await storage.write(key: "userType", value: authData.userInfo.userType);
+      await storage.write(key: "FIO", value: "${authData.userInfo.lastName} ${authData.userInfo.firstName} ${authData.userInfo.middleName}");
+
       emit(state.copyWith(authData: authData, isLoading: false));
     } catch (e) {}
   }
