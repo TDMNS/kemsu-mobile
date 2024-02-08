54961e7abe82b03ebfdb95853a4ed4e2263fd160
Oleg Kolbasa <773o@mail.ru>
2024-02-08 20:28:37 +0700

    Анимация иконок в таббаре

--- lib/UI/menu.dart
+++ lib/UI/menu.dart
@@ -1,24 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:kemsu_app/UI/views/news/news_view.dart';
 import 'package:kemsu_app/UI/views/profile/profile_view.dart';
 import 'package:kemsu_app/UI/views/schedule_new/schedule_screen.dart';
-
-import '../Configurations/localizable.dart';
 
 class MainMenu extends StatefulWidget {
   const MainMenu({super.key, required this.type});
   final int type;
+
   @override
   State<MainMenu> createState() => _MainMenuState();
 }
 
-class _MainMenuState extends State<MainMenu> {
+class _MainMenuState extends State<MainMenu> with TickerProviderStateMixin {
   int _selectedIndex = 0;
 
+  late List<AnimationController> _animationControllers;
+  late List<Animation<double>> _animations;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationControllers = List.generate(
+        2,
+            (index) => AnimationController(
+          vsync: this,
+          duration: const Duration(milliseconds: 300),
+        ));
+
+    _animations = _animationControllers
+        .map((controller) => Tween<double>(begin: 1.0, end: 1.2).animate(
+      CurvedAnimation(parent: controller, curve: Curves.elasticOut),
+    ))
+        .toList();
+
+    _animationControllers[_selectedIndex].forward();
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _animationControllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
   void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+    if (_selectedIndex != index) {
+      _animationControllers[_selectedIndex].reverse().then((_) {
+        _animationControllers[index].forward();
+      });
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
   }
 
   @override
@@ -26,27 +59,36 @@ class _MainMenuState extends State<MainMenu> {
     return Scaffold(
       body: IndexedStack(
         index: _selectedIndex,
-        children: const [ProfileView(), ScheduleScreen()],
+        children: const [
+          ProfileView(),
+          ScheduleScreen(),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Theme.of(context).primaryColor,
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.blue,
-        unselectedItemColor: Theme.of(context).canvasColor, //<-- Unselected text
+        unselectedItemColor: Theme.of(context).canvasColor,
         type: BottomNavigationBarType.fixed,
         onTap: _onItemTapped,
-        items: <BottomNavigationBarItem>[
+        items: [
           // BottomNavigationBarItem(
           //   icon: const Icon(Icons.newspaper),
           //   label: Localizable.pageNews,
           // ),
           BottomNavigationBarItem(
-            icon: const Icon(Icons.home),
-            label: Localizable.pageMain,
+            icon: ScaleTransition(
+              scale: _animations[0],
+              child: const Icon(Icons.home),
+            ),
+            label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: const Icon(Icons.schedule),
-            label: Localizable.pageSchedule,
+            icon: ScaleTransition(
+              scale: _animations[1],
+              child: const Icon(Icons.schedule),
+            ),
+            label: 'Schedule',
           ),
         ],
       ),
