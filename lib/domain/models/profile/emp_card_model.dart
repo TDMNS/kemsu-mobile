8fa7d6118fae3eeac01be585a43066d6f5acea12
Kirill Mishunin <69361708+SeriousVenom@users.noreply.github.com>
2024-05-08 12:21:53 +0700

    Убраны неиспользуемые поля из моделей авторизации и профиля, оставшиеся поля сделаны опциональными

--- lib/domain/models/profile/emp_card_model.dart
+++ lib/domain/models/profile/emp_card_model.dart
@@ -5,13 +5,13 @@ part 'emp_card_model.g.dart';
 @JsonSerializable()
 class EmpCardModel {
   @JsonKey(name: "success")
-  final bool success;
+  final bool? success;
   @JsonKey(name: "empList")
-  final List<EmpList> empList;
+  final List<EmpList>? empList;
 
   EmpCardModel({
-    required this.success,
-    required this.empList,
+    this.success,
+    this.empList,
   });
 
   factory EmpCardModel.fromJson(Map<String, dynamic> json) => _$EmpCardModelFromJson(json);
@@ -20,29 +20,11 @@ class EmpCardModel {
 
 @JsonSerializable()
 class EmpList {
-  @JsonKey(name: "LOGIN")
-  final String login;
-  @JsonKey(name: "LAST_NAME")
-  final String lastName;
-  @JsonKey(name: "FIRST_NAME")
-  final String firstName;
-  @JsonKey(name: "MIDDLE_NAME")
-  final String middleName;
-  @JsonKey(name: "POST_NAME")
-  final dynamic postName;
   @JsonKey(name: "DEP")
-  final String dep;
-  @JsonKey(name: "STATUS")
-  final String status;
+  final String? dep;
 
   EmpList({
-    required this.login,
-    required this.lastName,
-    required this.firstName,
-    required this.middleName,
-    required this.postName,
-    required this.dep,
-    required this.status,
+    this.dep,
   });
 
   factory EmpList.fromJson(Map<String, dynamic> json) => _$EmpListFromJson(json);
--- lib/domain/models/profile/stud_card_model.dart
+++ lib/domain/models/profile/stud_card_model.dart
@@ -5,59 +5,23 @@ part 'stud_card_model.g.dart';
 
 @JsonSerializable()
 class StudCardModel extends Equatable {
-  @JsonKey(name: "ID")
-  final int id;
-  @JsonKey(name: "FACULTY_ID")
-  final int facultyId;
-  @JsonKey(name: "SPECIALITY_ID")
-  final int specialityId;
-  @JsonKey(name: "PERSON_ID")
-  final int personId;
-  @JsonKey(name: "COURS")
-  final int cours;
-  @JsonKey(name: "GROUP_ID")
-  final int groupId;
   @JsonKey(name: "GROUP_NAME")
-  final String groupName;
+  final String? groupName;
   @JsonKey(name: "FACULTY")
-  final String faculty;
+  final String? faculty;
   @JsonKey(name: "SPECIALITY")
-  final String speciality;
-  @JsonKey(name: "SPECIALIZATION")
-  final dynamic specialization;
-  @JsonKey(name: "QUALIFICATION")
-  final String qualification;
+  final String? speciality;
   @JsonKey(name: "LEARN_FORM")
-  final String learnForm;
-  @JsonKey(name: "STATUS_STR")
-  final String statusStr;
+  final String? learnForm;
   @JsonKey(name: "FINFORM")
-  final String finform;
-  @JsonKey(name: "BEP")
-  final String bep;
-  @JsonKey(name: "START_YEAR")
-  final int startYear;
-  @JsonKey(name: "ADMISSION_DATE")
-  final String admissionDate;
+  final String? finform;
 
   const StudCardModel({
-    required this.id,
-    required this.facultyId,
-    required this.specialityId,
-    required this.personId,
-    required this.cours,
-    required this.groupId,
-    required this.groupName,
-    required this.faculty,
-    required this.speciality,
-    required this.specialization,
-    required this.qualification,
-    required this.learnForm,
-    required this.statusStr,
-    required this.finform,
-    required this.bep,
-    required this.startYear,
-    required this.admissionDate,
+    this.groupName,
+    this.faculty,
+    this.speciality,
+    this.learnForm,
+    this.finform,
   });
 
   factory StudCardModel.fromJson(Map<String, dynamic> json) => _$StudCardModelFromJson(json);
@@ -66,22 +30,10 @@ class StudCardModel extends Equatable {
 
   @override
   List<Object?> get props => [
-        id,
-        facultyId,
-        specialityId,
-        personId,
-        cours,
-        groupId,
         groupName,
         faculty,
         speciality,
-        specialization,
-        qualification,
         learnForm,
-        statusStr,
         finform,
-        bep,
-        startYear,
-        admissionDate,
       ];
 }
